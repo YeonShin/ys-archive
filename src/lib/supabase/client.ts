df1db75352b4091f68535ba651858c33489d3d60
YeonShin <yeonnim82@gmail.com>
2026-07-23 17:53:58 +0900

    chore: 환경 변수 접근 시 Non-Null 선언 대신 옵셔널 체이닝을 사용하도록 수정 (#25)

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -3,8 +3,8 @@ import { createBrowserClient } from '@supabase/ssr';
 import { Database } from '@/types/database.types';
 
 export function createClient() {
-  return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
+
+  return createBrowserClient<Database>(supabaseUrl, supabaseAnonKey);
 }
