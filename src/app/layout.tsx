b58f05009a9305db14c1d61ee6d249d8836c439b
YeonShin <yeonnim82@gmail.com>
2026-07-26 19:29:15 +0900

    feat: 다크 모드 토글 컴포넌트 추가 및 더 이상 필요 없어진 .gitkeep 파일 제거 (#30) - 다크모드 localstorage 값을 토글하는 컴포넌트 추가 - global.css 에서 잘못된 다크모드 우선순위 설정 수정 - 사이트 메타데이터 내용 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,28 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/providers/theme-provider";
-import { cn } from "@/lib/utils";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono, Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+import ThemeToggle from '@/components/common/ThemeToggle';
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/providers/theme-provider';
+
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Developer Portfolio",
-  description: "Web Developer Portfolio built with Next.js and Supabase",
+  title: '프론트엔드 김연신 | 포트폴리오',
+  description: '프론트엔드 개발자 김연신 포트폴리오 입니다.',
+  keywords: ['프론트엔드', '개발자'],
 };
 
 const themeInitializerScript = `
@@ -44,11 +48,11 @@ export default function RootLayout({
     <html
       lang="en"
       className={cn(
-        "h-full",
-        "antialiased",
+        'h-full',
+        'antialiased',
         geistSans.variable,
         geistMono.variable,
-        "font-sans",
+        'font-sans',
         inter.variable,
       )}
       suppressHydrationWarning
@@ -57,7 +61,9 @@ export default function RootLayout({
         <script dangerouslySetInnerHTML={{ __html: themeInitializerScript }} />
       </head>
       <body className="flex min-h-full flex-col">
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          {children} <ThemeToggle />
+        </ThemeProvider>
       </body>
     </html>
   );
