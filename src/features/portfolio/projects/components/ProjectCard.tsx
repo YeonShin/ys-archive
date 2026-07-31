6b66b6abd4f7e4255a3b2696527eced0f468be89
YeonShin <yeonnim82@gmail.com>
2026-07-31 16:35:32 +0900

    feat: shadcn/ui의 sheet 컴포넌트를 활용하여 이미지 패널을 트리거로 Sheet 컴포넌트가 열리도록 설정 (#36) - 기존 불필요한 handleKeyDown 이벤트 핸들러 제거 - SheetTrigger가 버튼 기반임에 따라 이에 맞춰 기존 웹접근성 및 태그들 수정

--- src/features/portfolio/projects/components/ProjectCard.tsx
+++ src/features/portfolio/projects/components/ProjectCard.tsx
@@ -3,6 +3,7 @@
 import { Variants } from 'motion';
 import { motion } from 'motion/react';
 
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { cn } from '@/lib/utils';
 
 import { Project } from '../type';
@@ -17,23 +18,39 @@ interface ProjectCardProps {
 
 const ProjectCard = ({ project, isEven, variant }: ProjectCardProps) => {
   return (
-    <motion.div
-      className="flex flex-col overflow-hidden rounded-2xl shadow-xl"
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true, amount: 0.5 }}
-      variants={variant}
-    >
-      <div className={cn('flex flex-col-reverse', isEven ? 'md:flex-row-reverse' : 'md:flex-row')}>
-        <div className="flex w-full md:w-2/5">
-          <ProjectCardInfoPanel project={project} />
-        </div>
+    <Sheet>
+      <motion.div
+        className="flex flex-col overflow-hidden rounded-2xl shadow-xl"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={variant}
+      >
+        <div
+          className={cn('flex flex-col-reverse', isEven ? 'md:flex-row-reverse' : 'md:flex-row')}
+        >
+          <div className="flex w-full md:w-2/5">
+            <ProjectCardInfoPanel project={project} />
+          </div>
 
-        <div className="flex w-full md:w-3/5">
-          <ProjectCardImagePanel project={project} />
+          <SheetTrigger asChild>
+            <button
+              type="button"
+              aria-label={`${project.title} 프로젝트 자세히 보기`}
+              className="group flex w-full cursor-pointer text-left outline-none md:w-3/5"
+            >
+              <ProjectCardImagePanel project={project} />
+            </button>
+          </SheetTrigger>
         </div>
-      </div>
-    </motion.div>
+      </motion.div>
+
+      <SheetContent
+        side="right"
+        showCloseButton={false}
+        className="w-full overflow-y-auto border-none data-[side=right]:sm:max-w-2xl"
+      ></SheetContent>
+    </Sheet>
   );
 };
 
