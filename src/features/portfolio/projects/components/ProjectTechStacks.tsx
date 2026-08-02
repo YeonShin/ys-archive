783d62009325bf6e27dc321bd45d33a157b92f53
YeonShin <yeonnim82@gmail.com>
2026-08-02 17:05:18 +0900

    feat: 프로젝트 상세 패널 요소들(프로젝트 소개, 기술스택) UI 구현 (#36) - 기술스택 선정 이유를 prop으로 받아 툴팁 표시 여부에 따른 툴팁 표시를 구현 - 프로젝트 설명, 기술스택 영역 UI 구현

--- src/features/portfolio/projects/components/ProjectTechStacks.tsx
+++ src/features/portfolio/projects/components/ProjectTechStacks.tsx
@@ -1,25 +1,42 @@
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { cn } from '@/lib/utils';
 
 import { ProjectTechStack } from '../type';
 
 interface ProjecTechStacksProps {
   techStacks?: ProjectTechStack[];
+  isReasonVisible?: boolean;
 }
 
-const ProjectTechStacks = ({ techStacks }: ProjecTechStacksProps) => {
+const ProjectTechStacks = ({ techStacks, isReasonVisible = false }: ProjecTechStacksProps) => {
   return (
-    <div className="flex flex-1 flex-wrap justify-start gap-1.5 md:flex-initial">
-      {techStacks?.map((tech) => (
-        <span
-          key={tech.name}
-          className={cn(
-            'bg-brand-neutral-dark text-brand-neutral-muted rounded-lg px-2 py-1.5 font-mono text-xs transition-all duration-300',
-          )}
-        >
-          {tech.name}
-        </span>
-      ))}
-    </div>
+    <TooltipProvider>
+      <div className="flex flex-1 flex-wrap justify-start gap-1.5 md:flex-initial">
+        {techStacks?.map((tech) => (
+          <Tooltip key={tech.name}>
+            <TooltipTrigger asChild>
+              <span
+                className={cn(
+                  'bg-brand-neutral-dark text-brand-neutral-muted rounded-lg px-2 py-1.5 font-mono text-xs transition-all duration-300',
+                  isReasonVisible && 'hover:bg-brand-primary',
+                )}
+              >
+                {tech.name}
+              </span>
+            </TooltipTrigger>
+            {isReasonVisible && (
+              <TooltipContent
+                side="top"
+                align="start"
+                className="bg-brand-neutral-dark fill-brand-neutral-dark flex flex-col items-center font-mono transition-all duration-300"
+              >
+                <p className="text-brand-neutral-light font-bold">{tech.reason}</p>
+              </TooltipContent>
+            )}
+          </Tooltip>
+        ))}
+      </div>
+    </TooltipProvider>
   );
 };
 
