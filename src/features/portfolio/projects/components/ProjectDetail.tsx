ab4717eb65a6e7f9e4d28cbacce7a86953005d87
YeonShin <yeonnim82@gmail.com>
2026-08-02 17:05:18 +0900

    feat: 프로젝트 상세 패널 요소들(프로젝트 소개, 기술스택) UI 구현 (#36) - 기술스택 선정 이유를 prop으로 받아 툴팁 표시 여부에 따른 툴팁 표시를 구현 - 프로젝트 설명, 기술스택 영역 UI 구현

--- src/features/portfolio/projects/components/ProjectDetail.tsx
+++ src/features/portfolio/projects/components/ProjectDetail.tsx
@@ -7,10 +7,32 @@ import { SheetClose, SheetHeader, SheetTitle } from '@/components/ui/sheet';
 
 import { Project } from '../type';
 import ImageCarousel from './ImageCarousel';
+import ProjectExternalLinks from './ProjectExternalLinks';
+import ProjectMetaInfo from './ProjectMetaInfo';
+import ProjectSubtitle from './ProjectSubtitle';
+import ProjectTechStacks from './ProjectTechStacks';
+import ProjectTitleHeader from './ProjectTitleHeader';
 
 interface ProjectDetailProps {
   project: Project;
 }
+
+interface DetailHeaderProps {
+  title: string;
+}
+
+const DetailHeader = ({ title }: DetailHeaderProps) => {
+  return (
+    <header>
+      <div className="mb-4 flex items-center gap-3">
+        <div className="bg-brand-primary h-5 w-1 shrink-0 rounded-full" />
+        <h3 className="text-brand-neutral-dark font-bold">{title}</h3>
+      </div>
+
+      <div className="bg-brand-secondary/30 h-px w-full rounded-full" />
+    </header>
+  );
+};
 
 const ProjectDetail = ({ project }: ProjectDetailProps) => {
   return (
@@ -32,14 +54,37 @@ const ProjectDetail = ({ project }: ProjectDetailProps) => {
         </SheetClose>
       </SheetHeader>
 
-      <article className="flex flex-col gap-4 px-8">
+      <article className="flex flex-col gap-8 px-8">
         {/* 이미지 캐러솔 영역 */}
         <ImageCarousel images={project.images} />
         {/* 프로젝트 설명 영역 */}
+        <div className="bg-brand-neutral-muted flex w-full flex-col justify-center gap-4 rounded-2xl px-5 py-8">
+          <ProjectTitleHeader title={project.title} status={project.status} />
+
+          {project.subtitle && <ProjectSubtitle subtitle={project.subtitle} />}
+
+          <ProjectMetaInfo
+            startedAt={project.startedAt}
+            endedAt={project.endedAt}
+            role={project.role}
+          />
+
+          <ProjectExternalLinks links={project.links} />
+        </div>
 
         {/* 프로젝트 소개 영역 */}
-
+        <div className="flex flex-col gap-4">
+          <DetailHeader title="프로젝트 소개" />
+          <div className="leading-8">{project.description}</div>
+        </div>
         {/* 기술 스택 영역 */}
+        <div className="flex flex-col gap-4">
+          <DetailHeader title="기술 스택" />
+          <ProjectTechStacks techStacks={project.techStacks} isReasonVisible={true} />
+          <div className="text-brand-secondary/70 text-sm font-light">
+            각 기술 위에 마우스를 올리면 선택 이유를 확인할 수 있습니다
+          </div>
+        </div>
 
         {/* 아키텍처 영역 */}
 
