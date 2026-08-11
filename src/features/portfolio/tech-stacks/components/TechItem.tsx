6d5ce105dd800934dd8457ce1fba48ea792a953b
YeonShin <yeonnim82@gmail.com>
2026-08-11 15:34:22 +0900

    refactor: 기존 포트폴리오 페이지의 TechItem.tsx의 아이콘 매핑 로직을 iconMap 공통 유틸 함수를 import 하여 사용하도로 수정 (#60)

--- src/features/portfolio/tech-stacks/components/TechItem.tsx
+++ src/features/portfolio/tech-stacks/components/TechItem.tsx
@@ -1,171 +1,10 @@
 'use client';
 
-import { FaAws, FaDatabase, FaGoogle, FaJava, FaServer, FaSlack } from 'react-icons/fa';
-import {
-  SiAngular,
-  SiApache,
-  SiApachekafka,
-  SiApollographql,
-  SiC,
-  SiCplusplus,
-  SiCypress,
-  SiDart,
-  SiDjango,
-  SiDocker,
-  SiDotnet,
-  SiElasticsearch,
-  SiEslint,
-  SiExpress,
-  SiFastapi,
-  SiFigma,
-  SiFirebase,
-  SiFlutter,
-  SiGit,
-  SiGithub,
-  SiGithubactions,
-  SiGo,
-  SiGraphql,
-  SiHtml5,
-  SiJavascript,
-  SiJest,
-  SiJira,
-  SiKotlin,
-  SiKubernetes,
-  SiLaravel,
-  SiLinux,
-  SiMariadb,
-  SiMongodb,
-  SiMysql,
-  SiNestjs,
-  SiNextdotjs,
-  SiNginx,
-  SiNodedotjs,
-  SiNotion,
-  SiPhp,
-  SiPostgresql,
-  SiPrettier,
-  SiPrisma,
-  SiPython,
-  SiRabbitmq,
-  SiReact,
-  SiRedis,
-  SiRuby,
-  SiRust,
-  SiSpring,
-  SiSpringboot,
-  SiSqlite,
-  SiStorybook,
-  SiSupabase,
-  SiSvelte,
-  SiSwift,
-  SiTailwindcss,
-  SiTypescript,
-  SiUbuntu,
-  SiVercel,
-  SiVite,
-  SiVuedotjs,
-  SiWebpack,
-} from 'react-icons/si';
-
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
+import { iconMap, normalizeIconName } from '@/lib/iconMap';
 import { cn } from '@/lib/utils';
 
 import { TechItem as TechItemType } from '../types';
-
-// DB에 저장된 이름(React, Next.js 등)을 소문자와 숫자만 남겨 정규화한 키로 매핑
-// 예: "Next.js" -> "nextjs", "Node.js" -> "nodejs", "AWS" -> "aws"
-const iconMap: Record<string, React.ElementType> = {
-  react: SiReact,
-  nextjs: SiNextdotjs,
-  typescript: SiTypescript,
-  javascript: SiJavascript,
-  tailwindcss: SiTailwindcss,
-  html: SiHtml5,
-  html5: SiHtml5,
-  nodejs: SiNodedotjs,
-  express: SiExpress,
-  nestjs: SiNestjs,
-  python: SiPython,
-  django: SiDjango,
-  fastapi: SiFastapi,
-  postgresql: SiPostgresql,
-  mysql: SiMysql,
-  mongodb: SiMongodb,
-  redis: SiRedis,
-  docker: SiDocker,
-  kubernetes: SiKubernetes,
-  githubactions: SiGithubactions,
-  vercel: SiVercel,
-  figma: SiFigma,
-  git: SiGit,
-  github: SiGithub,
-  supabase: SiSupabase,
-
-  // 백엔드 & 언어
-  java: FaJava,
-  spring: SiSpring,
-  springboot: SiSpringboot,
-  c: SiC,
-  cplusplus: SiCplusplus,
-  cpp: SiCplusplus,
-  dotnet: SiDotnet,
-  go: SiGo,
-  golang: SiGo,
-  rust: SiRust,
-  ruby: SiRuby,
-  php: SiPhp,
-  laravel: SiLaravel,
-  swift: SiSwift,
-  kotlin: SiKotlin,
-
-  // 프론트엔드 & 모바일
-  dart: SiDart,
-  flutter: SiFlutter,
-  vue: SiVuedotjs,
-  vuejs: SiVuedotjs,
-  angular: SiAngular,
-  svelte: SiSvelte,
-  graphql: SiGraphql,
-  apollo: SiApollographql,
-
-  // DB & 인프라
-  prisma: SiPrisma,
-  linux: SiLinux,
-  ubuntu: SiUbuntu,
-  nginx: SiNginx,
-  apache: SiApache,
-  firebase: SiFirebase,
-  sqlite: SiSqlite,
-  mariadb: SiMariadb,
-  oracle: FaDatabase,
-  elasticsearch: SiElasticsearch,
-  rabbitmq: SiRabbitmq,
-  kafka: SiApachekafka,
-
-  // 테스팅 & 도구
-  jest: SiJest,
-  cypress: SiCypress,
-  storybook: SiStorybook,
-  webpack: SiWebpack,
-  vite: SiVite,
-  eslint: SiEslint,
-  prettier: SiPrettier,
-  jira: SiJira,
-  slack: FaSlack,
-  notion: SiNotion,
-
-  // 폴백/추가 아이콘들
-  aws: FaAws,
-  amazonaws: FaAws,
-  amazonwebservices: FaAws,
-
-  google: FaGoogle,
-  googlecloud: FaGoogle,
-  gcp: FaGoogle,
-
-  database: FaDatabase,
-  server: FaServer,
-};
 
 interface TechItemProps {
   tech: TechItemType;
@@ -173,9 +12,7 @@ interface TechItemProps {
 }
 
 const TechItem = ({ tech, isFilteredOut }: TechItemProps) => {
-  const normalize = (name: string) => name.toLowerCase().replace(/[^a-z0-9]/g, '');
-
-  const searchKey = tech.icon ? normalize(tech.icon) : normalize(tech.name);
+  const searchKey = tech.icon ? normalizeIconName(tech.icon) : normalizeIconName(tech.name);
   const IconComponent = iconMap[searchKey] || null;
 
   return (
