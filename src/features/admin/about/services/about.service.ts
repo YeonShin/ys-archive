f64ccac3e86ab762b058bb3eaa4678ea2dc282e4
YeonShin <yeonnim82@gmail.com>
2026-08-12 15:42:11 +0900

    refactor: 연락처 제거 쿼리를 날릴 때 id가 dummy가 아닌 것 외 삭제 대신 null이 아닌 모든 것을 지우도록 수정 (#68)

--- src/features/admin/about/services/about.service.ts
+++ src/features/admin/about/services/about.service.ts
@@ -67,8 +67,9 @@ export const updateAboutData = async (data: AboutFormData) => {
         .not('id', 'in', `(${existingContactIds.join(',')})`);
       deleteError = error;
     } else {
-      // 모든 연락처를 지운 경우
-      const { error } = await supabase.from('contact').delete().neq('id', 'dummy');
+      // 모든 기존 연락처를 지운 경우 (새로 추가만 있거나, 아예 0개인 경우)
+      // id 컬럼이 uuid이므로 'dummy' 문자열을 비교하면 에러 발생. null이 아닌 모든 레코드를 삭제
+      const { error } = await supabase.from('contact').delete().not('id', 'is', null);
       deleteError = error;
     }
 
